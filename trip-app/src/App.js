f670d7483a0036f48114af8d6a5eef24c24d9e37
Alina Romanova <alinakhraban@gmail.com>
2023-08-02 22:32:15 +0300

    added components

--- trip-app/src/App.js
+++ trip-app/src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import Modal from './components/Modal/Modal';
+import FormSearch from './components/FormSearch/FormSearch';
+
+
+
 function App() {
   return (
     <div>
   <h1>hello world</h1>
+<FormSearch></FormSearch>
+  <Modal></Modal>
     </div>
   );
 }
---
+++ trip-app/src/components/FormSearch/FormSearch.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import css from '../../components/FormSearch/FormSearch.module.css';
+import iconSearch from '../../images/searchIcon.svg';
+import iconClose from '../../images/x-circle.svg';
+
+
+const FormSearch = ({ onSubmit, onChange, value, onClose, isHiden }) => {
+  return (
+<form onSubmit={onSubmit}>
+      <label htmlFor="search">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          value={value}
+          placeholder="Search your trip"
+          onChange={onChange}
+        />
+        {!isHiden ? (
+          <button className={css.searchButton}  disabled={!value}>
+            <svg  className={css.icon} src={iconSearch} />
+          </button>
+        ) : (
+          <button className={css.closeButton} type="button" onClick={onClose}>
+            <svg className={css.icon}  src={iconClose} />
+          </button>
+        )}
+      </label>
+    </form>
+  )
+}
+
+export default FormSearch
